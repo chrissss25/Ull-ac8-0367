85d8ecb8de9fa095c6f6922b322e7b1555f6ea73
CHRISTIAN_DAVID_HEREDIA_HEREDIA <a.21308051280367@cbtis128.edu.mx>
2024-04-26 17:42:02 +0000

    act

--- lib/main.dart
+++ lib/main.dart
@@ -3,40 +3,81 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
+  const MyApp({Key? key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Apple',
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
+  const MyHomePage({Key? key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
+        title: Text('Apple'),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              child: Text(
+                'Celulares Apple',
+              ),
+              decoration: BoxDecoration(
+                color: Color(0xffd9d9d9),
+              ),
+            ),
+            buildListTile(context, 'iPhoneXR', 'assets/image1.jpg'),
+            buildListTile(context, 'iPhone11', 'assets/image2.jpg'),
+            buildListTile(context, 'iPhone12', 'assets/image3.jpg'),
+            buildListTile(context, 'iPhone13', 'assets/image4.jpg'),
+            buildListTile(context, 'iPhone14', 'assets/image5.jpg'),
+            buildListTile(context, 'iPhone15', 'assets/image6.jpg'),
+          ],
+        ),
+      ),
+    );
+  }
+
+  ListTile buildListTile(BuildContext context, String title, String imagePath) {
+    return ListTile(
+      leading: CircleAvatar(
+        backgroundImage: AssetImage(imagePath),
+      ),
+      title: Text(title),
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+              builder: (context) => ImageScreen(imagePath: imagePath)),
+        );
+      },
+    );
+  }
+}
+
+class ImageScreen extends StatelessWidget {
+  final String imagePath;
+
+  const ImageScreen({Key? key, required this.imagePath}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Christian Heredia 0367'),
       ),
       body: Center(
-        child: Text(
-          'Hello, World!',
-        ),
+        child: Image.asset(imagePath),
       ),
     );
   }
